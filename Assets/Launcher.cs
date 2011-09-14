db2c3f2450ac4b262dbfe6505027a716001d31ab
George Buckenham <v.twenty.one@gmail.com>
2011-09-14 22:19:32 +0100

    uninformative commit message

--- Assets/Launcher.cs
+++ Assets/Launcher.cs
@@ -5,6 +5,9 @@ using System.Diagnostics;
 using System.IO;
 using System;
 using LitJson;
+using System.Windows.Forms;
+
+
 
 public class Launcher : MonoBehaviour {
 
@@ -12,6 +15,7 @@ public class Launcher : MonoBehaviour {
 	
 	public Process launched = null;
 	
+	KeyboardHook hook;
 	
 	public class Game{
 		public string author;
@@ -22,37 +26,45 @@ public class Launcher : MonoBehaviour {
 		public MovieTexture splashVideo = null;
 		public bool splashIsVideo = false;
 		
-		public Process process = new Process();
+		public Process process = null;
 		
 		public IEnumerator LoadTexture(){
+			splashScreenPath = "file://" + splashScreenPath; 
+			
 			if (splashIsVideo){
 				WWW www = new WWW(splashScreenPath);
 				yield return www;
-				splashVideo = www.movie;
+				if (www.error == null){
+					splashVideo = www.movie;
+				}
 			}
 			else {
 				WWW www = new WWW(splashScreenPath);
 				yield return www;
-				splashScreen = www.texture;
+				if (www.error == null){
+					splashScreen = www.texture;
+				}
 			}
+			
 			
 		}
 		
 		public void LaunchGame () {
 			UnityEngine.Debug.Log("game launching");
+			process = new Process();
 			process.StartInfo.FileName = filePath;
 			process.Start();
 			
 			//how do Events work in c#?  - trigger on process.Exited , assuming Unity is happy to run in the backround.
 			
 			
-			Screen.fullScreen = false;
+			UnityEngine.Screen.fullScreen = false;
 			
 		}
 		
 		public void OnGameExited(){
 			UnityEngine.Debug.Log("game exited");
-			Screen.fullScreen = true;
+			UnityEngine.Screen.fullScreen = true;
 		}
 		
 	}
@@ -70,11 +82,25 @@ public class Launcher : MonoBehaviour {
 	
 	public int currentGameI = 0;
 	
+	public Texture2D errorTex;
+	public GUIText errorGUIText;
+	
+	public Texture2D imgMissing;
+	
+	public bool firstImageLoaded = false;
+	
 	public string localJsonPath;
 	// Use this for initialization
 	void Start () {
-		
-		configFile = ReadJson(localJsonPath);
+		UnityEngine.Screen.showCursor = false;
+		try{
+			configFile = ReadJson(localJsonPath);
+		} catch (Exception e){
+			
+			
+			guiTexture.texture = errorTex;
+			errorGUIText.text = e.Message;
+		}
 		
 		
 		foreach (Game game in configFile.games) {
@@ -83,22 +109,27 @@ public class Launcher : MonoBehaviour {
 		
 		currentGameI = 0;
 		
+		hook = new KeyboardHook();
+		// register the event that is fired after the key press.
+        hook.KeyPressed += 
+            new EventHandler<KeyPressedEventArgs>(hook_KeyPressed);
+        // register the control + alt + F12 combination as hot key.
+        hook.RegisterHotKey(ModifierKeys.None, 
+            Keys.F11);
 		
 		//LaunchFile( "C:\\Windows\\system32\\calc.exe");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-			try {
+		try {
 			if (configFile.games[currentGameI].process.HasExited){ //believe me, i know this isn't the way to do this - but i just want this to work and go to bed
 				configFile.games[currentGameI].OnGameExited();
 			}
-			} catch (Exception) {
-				
-			}
-
+		} catch (Exception) {
+		}
 		
-		if (guiTexture.texture == null){ //to handle rendering once it's loaded
+		if (!firstImageLoaded && (configFile.games[currentGameI].splashScreen != null || configFile.games[currentGameI].splashVideo != null)){ //to handle rendering once it's loaded
 			ChangeTexture();
 		}
 		
@@ -136,11 +167,19 @@ public class Launcher : MonoBehaviour {
 	}
 	
 	void ChangeTexture(){
+		
+		
+		
 		Game game = configFile.games[currentGameI];
 		
 		if (currentlyVideo){
-			MovieTexture guiMovieTex = (MovieTexture)guiTexture.texture;
-			guiMovieTex.Stop();
+			try {
+				MovieTexture guiMovieTex = (MovieTexture)guiTexture.texture;
+				guiMovieTex.Stop();
+			}
+			catch (Exception){
+				
+			}
 		}
 		
 		if (game.splashIsVideo){
@@ -151,18 +190,33 @@ public class Launcher : MonoBehaviour {
 				guiTexture.texture = game.splashVideo;
 				MovieTexture guiMovieTex = (MovieTexture)guiTexture.texture;
 				guiMovieTex.Play();
+				errorGUIText.text = "";
+				
 				guiMovieTex.loop = true;
+				firstImageLoaded = true;
+			}
+			else {
+				guiTexture.texture = imgMissing;
+				errorGUIText.text = game.name + "\n" + game.author;
+				
 				
 			}
 		}
 		else {
 		
-		if (game.splashScreen != null){
-				
+			if (game.splashScreen != null){
+					
 				currentlyVideo = false;
 				guiTexture.texture = game.splashScreen;
+				errorGUIText.text = "";
+				firstImageLoaded = true;
+			}
+			else {
+				guiTexture.texture = imgMissing;
+				errorGUIText.text = game.name + "\n" + game.author;
+			}
 		}
-		}
+		
 		
 	}
 	
@@ -175,7 +229,26 @@ public class Launcher : MonoBehaviour {
 	}
 	
 	
-	
+	void hook_KeyPressed(object sender, KeyPressedEventArgs e)
+    {
+        // show the keys pressed in a label.
+        print("hotkey pressed");
+        try {
+			if (configFile.games[currentGameI].process != null){ 
+        		if (!configFile.games[currentGameI].process.CloseMainWindow()){
+        			configFile.games[currentGameI].process.Kill();
+        			
+        		}
+        		
+				configFile.games[currentGameI].process.Close();
+				configFile.games[currentGameI].OnGameExited();
+				
+			}
+		} catch (Exception err) {
+			
+		}
+        
+    }
 
 	
 }
